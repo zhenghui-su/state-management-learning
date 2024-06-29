26d7e4af34f86e7606e5f3c352ecc60135eb8456
苏征辉 <343196323@qq.com>
2024-06-29 22:35:17 +0800

    🎉 init: 状态管理库学习初始化

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  projects: [
+    '<rootDir>/packages/zustand',
+    '<rootDir>/packages/jotai',
+    '<rootDir>/packages/valtio',
+    '<rootDir>/packages/react-query',
+  ], // Jest 会同时处理 projects 中指定的所有项目
+}
